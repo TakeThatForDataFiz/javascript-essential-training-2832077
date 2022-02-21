52c3bbb8d5e8e747cca14ddd6c33a29d57fff3d8
John Lunz <johnlunz8@gmail.com>
2022-02-21 16:15:22 -0500

    Completed Practice Exercise creating functions

--- Practice/08_06/script.js
+++ Practice/08_06/script.js
@@ -5,3 +5,28 @@
  * - Main function creates article element with data from object
  * - Helper function creates.
  */
+const mainBody = document.querySelector("main");
+
+function addUL(mainBody) {
+  let newUL = document.createElement("ul");
+  mainBody.appendChild(newUL);
+}
+
+const addItems = function (UL, numItems) {
+  for (let i = 0; i < numItems; i++) {
+    let newLI = document.createElement("li");
+    newLI.innerHTML = `Item ${i + 1}`;
+    UL.appendChild(newLI);
+  }
+};
+
+const popItem = (UL) => {
+  let poppedLI = document.querySelector("ul li:last-child");
+  poppedLI.parentElement.removeChild(poppedLI);
+  console.log(poppedLI);
+};
+
+addUL(mainBody);
+createdUL = document.querySelector("ul");
+addItems(createdUL, 5);
+popItem(createdUL);
