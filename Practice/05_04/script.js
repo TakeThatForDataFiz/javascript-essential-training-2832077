2d8339b42e25c8c1c3e77d2a469fb97dab8f5095
John Lunz <johnlunz8@gmail.com>
2022-02-21 12:23:27 -0500

    Added Color change to last Backpack using DOM

--- Practice/05_04/script.js
+++ Practice/05_04/script.js
@@ -2,3 +2,8 @@
  * Note: This file is intentionally empty.
  * You can use it to test your skills at traversing the DOM using JavaScript.
  */
+const mainElement = document.querySelector(".maincontent");
+
+const lastBackpack = mainElement.querySelector(".backpack:last-child");
+
+lastBackpack.style.backgroundColor = "purple";
