ceec637fea75972ebb20645fcee13fb2316fdc0e
John Lunz <johnlunz8@gmail.com>
2022-02-21 13:47:50 -0500

    Added Attribute property change with forEach loop

--- Practice/05_08/script.js
+++ Practice/05_08/script.js
@@ -3,3 +3,8 @@
  * You can use it to test your skills at adding, removing, or modifying
  * attributes, classes, and inline styles on elements.
  */
+const backpackImgs = document.querySelectorAll(".backpack__image");
+
+backpackImgs.forEach(
+  (backpack) => (backpack.querySelector("img").alt = "Image of Backpack")
+);
